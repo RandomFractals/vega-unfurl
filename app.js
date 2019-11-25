4b8eb11941f2e93a26a368c905622c6c6fe23a88
Taras Novak <taras.novak@randomfractals.com>
2019-11-25 07:03:59 -0600

    switched to using express for adding extra vega docs handlers later

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+// load .env config
+require('dotenv').config();
+
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { createEventAdapter } = require('@slack/events-api');
 const { WebClient } = require('@slack/web-api');
 const keyBy = require('lodash.keyby');
 const omit = require('lodash.omit');
 const mapValues = require('lodash.mapvalues');
 const vegaUtils = require('./vega-utils.js');
-
-// load .env config
-require('dotenv').config();
 
 // create Slack events adapter with envelope data and headers
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
@@ -17,7 +20,6 @@ const slackEvents = createEventAdapter(slackSigningSecret, {
 
 // create Slack web client
 const slack = new WebClient(process.env.SLACK_ACCESS_TOKEN);
-const port = process.env.PORT || 3000;
 
 // add link_shared Slack event handler
 slackEvents.on('link_shared', (event, body, headers) => {
@@ -50,10 +52,20 @@ slackEvents.on('error', (error) => {
   console.log(error);
 });
 
-(async () => {
-  // start the built-in server
-  const server = await slackEvents.start(port);
+// create express app
+const app = express();
 
+// add slack events listener
+app.use('/slack/events', slackEvents.requestListener());
+
+// mount json body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// start http server
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
   // log server started message
   console.log(`Listening for events on port: ${server.address().port}`);
-})();
+});
